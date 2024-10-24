7a3f5fe6c67c47897d69e8777dac271d7b954719
Dan5326 <127957242+D53261@users.noreply.github.com>
2024-10-24 15:24:41 -0300

    Update Registro.java

--- src/main/java/com/mycompany/pei/Registro.java
+++ src/main/java/com/mycompany/pei/Registro.java
@@ -1,22 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package com.mycompany.pei;
 
 import javax.swing.JOptionPane;
 import mapbd.Confirma;
 import objetoacesso.ConfirmaDAO;
 
-/**
- *
- * @author uept42-user
- */
 public class Registro extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Registro
-     */
+
     public Registro() {
         initComponents();
     }
