f136281388f95a6d691d132f57ab0fe3d7a5deff
Dan5326 <127957242+D53261@users.noreply.github.com>
2024-10-24 15:24:17 -0300

    Update Pei.java

--- src/main/java/com/mycompany/pei/Pei.java
+++ src/main/java/com/mycompany/pei/Pei.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.pei;
 
 import conexao.Conexao;
 
-/**
- *
- * @author uept42-user
- */
 public class Pei {
 
     public static void main(String[] args) {
